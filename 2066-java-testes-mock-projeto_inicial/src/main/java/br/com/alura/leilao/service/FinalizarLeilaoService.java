42f84823ad4167794c0ccb6a8f47c1380e6852a0
Michelle <michelledantasluiz@gmail.com>
2022-10-03 08:20:46 -0300

    Escrevendo um teste com mockito

--- 2066-java-testes-mock-projeto_inicial/src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
+++ 2066-java-testes-mock-projeto_inicial/src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
@@ -12,8 +12,13 @@ import br.com.alura.leilao.model.Leilao;
 @Service
 public class FinalizarLeilaoService {
 
-	@Autowired
 	private LeilaoDao leiloes;
+
+	@Autowired
+	public FinalizarLeilaoService(LeilaoDao leiloes) {
+		this.leiloes = leiloes;
+	}
+
 
 	public void finalizarLeiloesExpirados() {
 		List<Leilao> expirados = leiloes.buscarLeiloesExpirados();
